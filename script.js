bc22112a1949fdf467d37e15940ac251196276be
Soumya1743 <soumyarangaihagari043@gmail.com>
2025-04-17 18:54:10 +0530

    Final fix

---
+++ script.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    document.getElementById("loader").style.opacity = "0";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}, 4000);
